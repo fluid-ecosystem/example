eec4edcce422e2ff0b8384fd15a949083f0cf814
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-05-23 22:43:50 +0600

    [example]: using fluid sender;

--- example-send/Fluid.java
+++ example-send/Fluid.java
@@ -3,34 +3,18 @@ import org.apache.kafka.clients.producer.*;
 import java.util.Properties;
 
 public class Fluid {
-    public static void main(String[] args) {
-        Properties props = new Properties();
-        // props.put("bootstrap.servers", "localhost:9092");
-        props.put("bootstrap.servers", "kafka-cluster:9092");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
-        Producer<String, String> producer = new KafkaProducer<>(props);
-
+    public static void main(String[] args)  throws InterruptedException {
         try {
             for (int i = 0; i < 1000; i++) {
-                ProducerRecord<String, String> record = new ProducerRecord<>(
-                        "test-topic1", 
-                        "key-" + i, 
-                        "Message " + i
-                );
-                
-                producer.send(record, (metadata, exception) -> {
-                    if (exception == null) {
-                        System.out.printf("Sent message to partition %d, offset %d%n",
-                                metadata.partition(), metadata.offset());
-                    } else {
-                        System.err.println("Error sending message: " + exception.getMessage());
-                    }
-                });
+                String key = "key-" + i;
+                String message = "Message " + i;
+                KafkaMessenger.sendMessage("test-topic1", message);
             }
         } finally {
-            producer.close();
+            System.out.println("Shutting down Kafka producer...");
+            KafkaMessenger.shutdown();
+            System.out.println("Kafka producer shut down.");
         }
+        Thread.sleep(50000);
     }
 }

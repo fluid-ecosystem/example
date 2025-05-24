66ef0c0d4a67f690e448395009636dab39f9be68
Maifee Ul Asad <29339330+maifeeulasad@users.noreply.github.com>
2025-05-24 15:52:11 +0600

    [feature]: multiple handler in same service;
    as per https://github.com/fluid-ecosystem/fluid-builder/tree/b0a929e48a4de57e62117d686058e88b43544d0f

--- example-receive/MessageService.java
+++ example-receive/MessageService.java
@@ -1,8 +1,14 @@
 public class MessageService {
     @KafkaListener(topic = "test-topic1", groupId = "test-group")
-    public void handleMessage(String message) {
+    public void handleMessage1(String message) {
         // print time
-        System.out.println("Received message at " + System.currentTimeMillis());
-        System.out.println("Received: " + message);
+        System.out.println("1-Received message at " + System.currentTimeMillis());
+        System.out.println("1-Received: " + message);
+    }
+    @KafkaListener(topic = "test-topic2", groupId = "test-group")
+    public void handleMessage2(String message) {
+        // print time
+        System.out.println("2-Received message at " + System.currentTimeMillis());
+        System.out.println("2-Received: " + message);
     }
 }

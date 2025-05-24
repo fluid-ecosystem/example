e9dbad3fe065620a0cf6a0171538d55825fcea07
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-05-24 18:16:25 +0600

    [example]: using custom bootstrap server for listener;

--- example-receive/MessageService.java
+++ example-receive/MessageService.java
@@ -1,11 +1,11 @@
 public class MessageService {
-    @KafkaListener(topic = "test-topic1", groupId = "test-group")
+    @KafkaListener(topic = "test-topic1", groupId = "test-group", bootstrapServers = "kafka:9092")
     public void handleMessage1(String message) {
         // print time
         System.out.println("1-Received message at " + System.currentTimeMillis());
         System.out.println("1-Received: " + message);
     }
-    @KafkaListener(topic = "test-topic2", groupId = "test-group")
+    @KafkaListener(topic = "test-topic2", groupId = "test-group", bootstrapServers = "kafka:9092")
     public void handleMessage2(String message) {
         // print time
         System.out.println("2-Received message at " + System.currentTimeMillis());
